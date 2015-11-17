a13cd69fcb54d3cc8ed5a7ecea708861f17257fb
ivan vilches <ivan_vilches@hotmail.com>
2015-11-17 06:22:45 -0300

    materialize-masonry
    masonry grid with materialize framework from http://materializecss.com/

---
+++ index.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+  <title>Masonry Materialize</title>
+
+  <!-- CSS  -->
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+  <div class="section no-pad-bot">
+    <div class="container">
+      <br><br>
+    
+      <div class="row center">
+        <h5 class="header col s12 light">Masonry and Materialize</h5>
+      </div>
+    
+      <br><br>
+
+
+      <div id="masonry-grid" class="row">
+        <div class="col s12 m6 l4">
+          <div class="card">
+            <div class="card-image">
+              <img src="http://dummyimage.com/600x400/000/fff">
+            </div>
+          </div>
+        </div>
+         <div class="col s12 m6 l4">
+          <div class="card">
+            <div class="card-image">
+              <img src="http://dummyimage.com/700x800/000/fff">
+            </div>
+          </div>
+        </div>
+         <div class="col s12 m6 l4">
+          <div class="card">
+            <div class="card-image">
+              <img src="http://dummyimage.com/1000x900/000/fff">
+            </div>
+          </div>
+        </div>
+         <div class="col s12 m6 l4">
+          <div class="card">
+            <div class="card-image">
+              <img src="http://dummyimage.com/1000x400/000/fff">
+            </div>
+          </div>
+        </div>
+         <div class="col s12 m6 l4">
+          <div class="card">
+            <div class="card-image">
+              <img src="http://dummyimage.com/600x400/000/fff">
+            </div>
+          </div>
+        </div>
+         <div class="col s12 m6 l4">
+          <div class="card">
+            <div class="card-image">
+              <img src="http://dummyimage.com/700x400/000/fff">
+            </div>
+          </div>
+        </div>
+         <div class="col s12 m6 l4">
+          <div class="card">
+            <div class="card-image">
+              <img src="http://dummyimage.com/500x400/000/fff">
+            </div>
+          </div>
+        </div>
+         <div class="col s12 m6 l4">
+          <div class="card">
+            <div class="card-image">
+              <img src="http://dummyimage.com/1000x500/000/fff">
+            </div>
+          </div>
+        </div>
+         <div class="col s12 m6 l4">
+          <div class="card">
+            <div class="card-image">
+              <img src="http://dummyimage.com/800x600/000/fff">
+            </div>
+          </div>
+        </div>
+         <div class="col s12 m6 l4">
+          <div class="card">
+            <div class="card-image">
+              <img src="http://dummyimage.com/600x600/000/fff">
+            </div>
+          </div>
+        </div>
+         <div class="col s12 m6 l4">
+          <div class="card">
+            <div class="card-image">
+              <img src="http://dummyimage.com/800x1000/000/fff">
+            </div>
+          </div>
+        </div>
+                <div class="col s12 m6 l4">
+          <div class="card">
+            <div class="card-image">
+              <img src="http://dummyimage.com/600x400/000/fff">
+            </div>
+          </div>
+        </div>
+         <div class="col s12 m6 l4">
+          <div class="card">
+            <div class="card-image">
+              <img src="http://dummyimage.com/700x800/000/fff">
+            </div>
+          </div>
+        </div>
+         <div class="col s12 m6 l4">
+          <div class="card">
+            <div class="card-image">
+              <img src="http://dummyimage.com/1000x900/000/fff">
+            </div>
+          </div>
+        </div>
+         <div class="col s12 m6 l4">
+          <div class="card">
+            <div class="card-image">
+              <img src="http://dummyimage.com/1000x400/000/fff">
+            </div>
+          </div>
+        </div>
+         <div class="col s12 m6 l4">
+          <div class="card">
+            <div class="card-image">
+              <img src="http://dummyimage.com/600x400/000/fff">
+            </div>
+          </div>
+        </div>
+         <div class="col s12 m6 l4">
+          <div class="card">
+            <div class="card-image">
+              <img src="http://dummyimage.com/700x400/000/fff">
+            </div>
+          </div>
+        </div>
+         <div class="col s12 m6 l4">
+          <div class="card">
+            <div class="card-image">
+              <img src="http://dummyimage.com/500x400/000/fff">
+            </div>
+          </div>
+        </div>
+         <div class="col s12 m6 l4">
+          <div class="card">
+            <div class="card-image">
+              <img src="http://dummyimage.com/1000x500/000/fff">
+            </div>
+          </div>
+        </div>
+         <div class="col s12 m6 l4">
+          <div class="card">
+            <div class="card-image">
+              <img src="http://dummyimage.com/800x600/000/fff">
+            </div>
+          </div>
+        </div>
+         <div class="col s12 m6 l4">
+          <div class="card">
+            <div class="card-image">
+              <img src="http://dummyimage.com/600x600/000/fff">
+            </div>
+          </div>
+        </div>
+         <div class="col s12 m6 l4">
+          <div class="card">
+            <div class="card-image">
+              <img src="http://dummyimage.com/800x1000/000/fff">
+            </div>
+          </div>
+        </div>
+      
+      </div>
+    </div>
+  </div>
+
+
+
+  
+
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/bin/materialize.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/masonry/3.3.2/masonry.pkgd.min.js"></script>
+  <script src="js/init.js"></script>
+
+  </body>
+</html>
